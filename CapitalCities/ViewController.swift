35b1b47b30081724c59ed56a0f1163a5380a2a8b
ahmed tantawy <a7mad_gamal@live.com>
2021-04-06 01:10:40 +0200

    Initial Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Ahmed Tantawy on 06/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
